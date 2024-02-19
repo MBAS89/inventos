1fc70d49831b4f2acbd9ddef02c08b0f02409d48
MBAS89 <mohammedbadran1@hotmail.com>
2024-02-19 21:28:30 +0200

    Enhance employee popup UI for direct contract addition
    - Updated the popup UI for adding and editing employees
    - Implemented functionality to directly add employees with contracts

--- frontend/src/components/employees/AddAndEditEmployeesPopup.jsx
+++ frontend/src/components/employees/AddAndEditEmployeesPopup.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineCloseCircle } from "react-icons/ai"
 import { DropZone } from '../DropZone'
 
 export const AddAndEditEmpolyeesPopup = ({ setOpenPopup }) => {
-  return (
+    const [withContract, setWithContract] = useState(false)
+
+    return (
     <section className="overflow-auto bg-white left-[32%] top-[7%] h-[52rem] w-[45rem] border-gray-300 border-solid border-[1px] absolute rounded-lg shadow-2xl">
         <div className='relative w-full bg-black'>
             <AiOutlineCloseCircle onClick={() => setOpenPopup(false)} className='text-gray-600 rounded-full cursor-pointer bg-white text-[2rem]  hover:scale-105 absolute right-4 top-4'/>
         </div>
         <h2 className='text-[2.5rem] font-bold text-center text-gray-500 capitalize mt-12'>Add Employee</h2>
+        <div className='flex items-center w-[70%] mx-auto mt-3 gap-5'>
+            <button onClick={() => setWithContract(false)} className={`inline-block ${!withContract ? 'text-white bg-[#50B426]' : 'text-[#50B426] bg-white' } rounded border w-full border-[#50B426] px-12 py-4 text-sm font-medium active:bg-green-500 text-[1.3rem]`}>Without Contract</button>
+            <button onClick={() => setWithContract(true)} className={`inline-block ${withContract ? 'text-white bg-[#50B426]' : 'text-[#50B426] bg-white' } rounded border w-full border-[#50B426] px-12 py-4 text-sm font-medium active:bg-green-500 text-[1.3rem]`}>With Contract</button>
+        </div>
         <form className='flex flex-col gap-8 w-[70%] mx-auto mt-5 mb-10'>
             <DropZone className="border-[2px] border-dashed py-8 border-[#50B426] cursor-pointer w-[60%] text-center px-2" />
             <label htmlFor="FullName" className="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-[#50B426] focus-within:ring-1 focus-within:ring-[#50B426]">
@@ -28,6 +34,20 @@ export const AddAndEditEmpolyeesPopup = ({ setOpenPopup }) => {
                     <input type="text" id="Phone" placeholder="Phone" className="peer h-12 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
                         Phone
+                    </span>
+                </label>
+            </div>
+            <div className='flex items-center w-full gap-4'>
+                <label htmlFor="password" className="relative block overflow-hidden w-full rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-[#50B426] focus-within:ring-1 focus-within:ring-[#50B426]">
+                    <input type="text" id="password" name='password' placeholder="password" className="peer h-12 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
+                    <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+                        Password
+                    </span>
+                </label>
+                <label htmlFor="confirmPassword" className="relative block overflow-hidden w-full rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-[#50B426] focus-within:ring-1 focus-within:ring-[#50B426]">
+                    <input type="text" id="confirmPassword" name='confirmPassword' placeholder="confirmPassword" className="peer h-12 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
+                    <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+                        Confirm Password
                     </span>
                 </label>
             </div>
@@ -58,19 +78,23 @@ export const AddAndEditEmpolyeesPopup = ({ setOpenPopup }) => {
                 </label>
                 <select name="HeadlineAct" id="HeadlineAct" className="mt-1.5 p-4 w-full border border-gray-200 rounded-md text-gray-700 sm:text-sm">
                     <option value="">Please select</option>
-                    <option value="JM">Full Time</option>
-                    <option value="SRV">Part Time</option>
-                    <option value="JH">Remote</option>
+                    <option value="full-time">Full Time</option>
+                    <option value="part-time">Part Time</option>
+                    <option value="remote">Remote</option>
+                    <option value="temporary">Temporary</option>
+                    <option value="hybrid">Hybrid</option>
+                    <option value="contract-based">Contract Based</option>
+                    <option value="not-installed">Not Installed</option>
                 </select>
             </div>
             <div>
                 <label htmlFor="HeadlineAct" className="block text-sm font-medium text-gray-900">
-                    Contract
+                    Salary Type
                 </label>
                 <select name="HeadlineAct" id="HeadlineAct" className="mt-1.5 p-4 w-full border border-gray-200 rounded-md text-gray-700 sm:text-sm">
-                    <option value="">Please select</option>
-                    <option value="JM">Yes</option>
-                    <option value="SRV">No</option>
+                    <option value="">Hourly</option>
+                    <option value="JM">Monthly</option>
+                    <option value="SRV">Yearly</option>
                 </select>
             </div>
             <label htmlFor="Salary" className="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-[#50B426] focus-within:ring-1 focus-within:ring-[#50B426]">
@@ -79,7 +103,35 @@ export const AddAndEditEmpolyeesPopup = ({ setOpenPopup }) => {
                     Salary
                 </span>
             </label>
-            <button className="inline-block rounded border w-full border-[#50B426] px-12 py-4 text-sm font-medium text-[#50B426] hover:bg-[#50B426] hover:text-white focus:outline-none focus:ring active:bg-green-500 text-[1.3rem]">Add Employee</button>
+            {withContract && (
+                <div className='flex flex-col gap-3'>
+                    <hr className='w-full h-1 bg-[#50B426]'/>
+                    <h4>Contract Info:</h4>
+                    <div className='flex items-center w-full gap-4'>
+                        <label htmlFor="startDate" className="relative block overflow-hidden w-full rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-[#50B426] focus-within:ring-1 focus-within:ring-[#50B426]">
+                            <input type="date" id="startDate" name='startDate' placeholder="startDate" className="peer h-12 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
+                            <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+                                Start Date
+                            </span>
+                        </label>
+                        <label htmlFor="endDate" className="relative block overflow-hidden w-full rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-[#50B426] focus-within:ring-1 focus-within:ring-[#50B426]">
+                            <input type="date" id="endDate" name='endDate' placeholder="endDate" className="peer h-12 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
+                            <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+                                End Date
+                            </span>
+                        </label>
+                    </div>
+                    <label htmlFor="detailes" className="relative block overflow-hidden w-full rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-[#50B426] focus-within:ring-1 focus-within:ring-[#50B426]">
+                        <textarea rows="4" id="detailes" name='detailes' placeholder="detailes" className="peer resize-none  w-full border-none bg-transparent pt-5 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"></textarea>
+                        <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+                            Contract Detailes
+                        </span>
+                    </label>
+                </div>
+            )}
+            <button className="inline-block rounded border w-full border-[#50B426] px-12 py-4 text-sm font-medium text-[#50B426] active:bg-green-500 active:text-white text-[1.3rem]">
+                {withContract ? 'Add Employee With Contract' : 'Add Employee'}
+            </button>
         </form>
     </section>
   )
